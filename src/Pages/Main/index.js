2dd99509e6f569413c504ce672e3aafff6afac17
Leonardo <leonardovieira.x@hotmail.com>
2020-03-04 14:27:14 -0300

    Page_Main

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
   return (
-    <Title error={false}>
-      Main
-      <small>menor</small>
-    </Title>
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar um repósitorio" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
